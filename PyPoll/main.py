79655c249975cef5ebc5c66e35e1226eaf0207b0
shadisalama <54451010+shadisalama@users.noreply.github.com>
2019-12-17 06:13:58 -0800

    Update main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,14 @@
+import os
+import csv
 
+
+csvpath = os.path.join('Resources', 'election_data.csv')
+
+with open(csvpath, 'r') as file_handler:
+    lines = file_handler.read()
+
+    
+   
+    
+    print(lines)
+    print(type(lines))
