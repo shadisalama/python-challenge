ca79aac8688a0f3519d0bb73eb45639209e66d90
shadisalama <54451010+shadisalama@users.noreply.github.com>
2019-12-17 06:10:47 -0800

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,11 @@
+import os
+import csv
 
+
+csvpath = os.path.join('Resources', 'budget_data.csv')
+
+with open(csvpath, 'r') as file_handler:
+    lines = file_handler.read()
+
+    print(lines)
+    print(type(lines))
